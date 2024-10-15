d95cdbe864a3497638e2280d343703e129b0364a
solomon-winner <solyalew3@gmail.com>
2024-10-15 21:39:19 +0300

    ...

---
+++ src/components/atoms/input/input.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const InputDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-top: 15px;
+    font-size: 15px;
+`;
+
+export const Field = styled.div`
+    marin-top: 12px;
+    background-color: gray;
+    padding: 12px;
+    width: 2rem;
+    border-radius: 10px;
+`
