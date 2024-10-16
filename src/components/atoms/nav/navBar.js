7865b2173b0d581a06779db21e87adf0eebeee41
solomon-winner <solyalew3@gmail.com>
2024-10-16 21:09:02 +0300

    ...

--- src/components/atoms/nav/navBar.js
+++ src/components/atoms/nav/navBar.js
@@ -3,10 +3,25 @@ import styled from "styled-components";
 export const NavBar = styled.div`
     width: 500px;
     height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
     background-color: white;
     position: fixed;
     top: 15px;
     right: 10%;
     border-radius: 10rem; 
     box-shadow: 0 3px 5px rgb( 0 0 0 / 0.2)
+`
+export const NavLinks = styled.div`
+    color: green;
+    width: 80px;
+    height:40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    &:hover {
+    cursor: pointer;
+    }
 `
--- src/components/atoms/nav/navBar.jsx
+++ src/components/atoms/nav/navBar.jsx
@@ -1,9 +1,14 @@
-import { NavBar } from "./navBar";
+import { NavBar, NavLinks } from "./navBar";
 
  const Nav = () => {
     return (
         <NavBar>
-            
+            <NavLinks>Home</NavLinks>
+            <NavLinks>About</NavLinks>
+            <NavLinks>Testimonies</NavLinks>
+            <NavLinks>Songs</NavLinks>
+            <NavLinks>Contacts</NavLinks>
+
         </NavBar>
     )
  }
