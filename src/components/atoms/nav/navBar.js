27594e1fcefabcd4e37ef9cc6ab2f1ae8ae4f4bc
solomon-winner <solyalew3@gmail.com>
2024-10-16 20:59:32 +0300

    ...

--- src/components/atoms/nav/navBar.js
+++ src/components/atoms/nav/navBar.js
@@ -3,5 +3,10 @@ import styled from "styled-components";
 export const NavBar = styled.div`
     width: 500px;
     height: 50px;
-    
+    background-color: white;
+    position: fixed;
+    top: 15px;
+    right: 10%;
+    border-radius: 10rem; 
+    box-shadow: 0 3px 5px rgb( 0 0 0 / 0.2)
 `
--- src/components/organisms/banner/banner.jsx
+++ src/components/organisms/banner/banner.jsx
@@ -1,10 +1,12 @@
 import BannerDesc from "../../molecules/bannerDesc/bannerDesc.jsx";
 import BannerCards from "../../molecules/bannerCards/bannerCards.jsx";
 import {BannerDiv} from "./banner";
+import Nav from "../../atoms/nav/navBar.jsx";
 
 const Banner = () => {
     return (
         <BannerDiv>
+            <Nav/>
             <BannerDesc/>
             <BannerCards/>
         </BannerDiv>    );
