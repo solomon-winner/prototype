8341f51749687ccb1c7e31ce842d05fd98963e26
solomon-winner <solyalew3@gmail.com>
2024-10-18 15:40:57 +0300

    ...

--- src/components/organisms/testimonies/testimony.js
+++ src/components/organisms/testimonies/testimony.js
@@ -1,21 +1,44 @@
 import styled from "styled-components";
 
 export const TestimonyDiv = styled.div`
-    width: 100%;
+    width: 90%;
     height: fit-content;
     display: flex;
+    flex-direction: column;
     justify-content:center;
+    align-items: center;
+    gap: 30px;
     max-width: 100vw;
     box-sizing: border-box;
-    border: 1px solid black;
+    margin-top: 5%;
+    text-align: start;
 `
 
 export const TestimonyCards = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 5%;
-    width: 300px;
+    width: 100%;
+    // height: fit-content;
+    background-color: white;
     height: 200px;
-    border: 1px solid black;
+    box-shadow: 0 0 10px 0 rgba(0,0,0,0.1);
+    padding: 20px;
+`
+export const Title = styled.div`
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: green;
+    width: 100%;
+`
+export const TestifierName = styled.div`
+    font-size: 1rem;
+    color: green;
+    font-weight: bold;
+    padding: 10px;
+    display: flex;
+    gap: 10px;
+
+`
+export const Testimonial = styled.div`
+    font-size: 1rem;
+    color: green;
+    font-style: italic;
 `
--- src/components/organisms/testimonies/testimony.jsx
+++ src/components/organisms/testimonies/testimony.jsx
@@ -1,11 +1,84 @@
-import { TestimonyCards, TestimonyDiv } from "./testimony";
+import { TestifierName, Testimonial, TestimonyCards, TestimonyDiv, Title } from "./testimony";
 
 const Testimony = () => {
     return (
         <TestimonyDiv>
-            <TestimonyCards></TestimonyCards>
-            <TestimonyCards></TestimonyCards>
-            <TestimonyCards></TestimonyCards>
+            <Title>Testimonies</Title>
+            <TestimonyCards>
+                <TestifierName>
+                    Solomon yalew
+                </TestifierName>
+                <Testimonial>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                    Doloremque dicta culpa deserunt 
+                    ducimus quae assumenda voluptates 
+                    illo laborum aperiam vero doloribus molestias quisquam, 
+                    eos voluptas architecto ullam nobis iste mollitia?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Quam sint commodi consequuntur repudiandae dolorem optio minima ab, 
+                    hic vitae nisi aspernatur quod ipsam, iusto alias? Quibusdam accusamus
+                     quo necessitatibus voluptates?
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Amet et expedita dolores, repudiandae iure rerum enim 
+                      quasi placeat obcaecati minima a odio natus hic quas eveniet voluptatem fugiat. Ut, ex?
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias 
+                      amet perspiciatis quas, voluptatem ea fugit, 
+                      nulla necessitatibus eaque cumque, ratione tempore omnis
+                       alias cupiditate provident voluptates molestiae possimus minus nostrum?
+                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci eaque vero quae cupiditate dicta error! 
+                       Perferendis inventore excepturi cumque necessitatibus ullam, labore nesciunt ducimus deserunt mollitia nobis at praesentium illo.
+                </Testimonial>
+            </TestimonyCards>
+            <TestimonyCards>
+            <TestifierName>
+                    Solomon yalew
+                </TestifierName>
+                <Testimonial>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                    Doloremque dicta culpa deserunt 
+                    ducimus quae assumenda voluptates 
+                    illo laborum aperiam vero doloribus molestias quisquam, 
+                    eos voluptas architecto ullam nobis iste mollitia?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Quam sint commodi consequuntur repudiandae dolorem optio minima ab, 
+                    hic vitae nisi aspernatur quod ipsam, iusto alias? Quibusdam accusamus
+                     quo necessitatibus voluptates?
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Amet et expedita dolores, repudiandae iure rerum enim 
+                      quasi placeat obcaecati minima a odio natus hic quas eveniet voluptatem fugiat. Ut, ex?
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias 
+                      amet perspiciatis quas, voluptatem ea fugit, 
+                      nulla necessitatibus eaque cumque, ratione tempore omnis
+                       alias cupiditate provident voluptates molestiae possimus minus nostrum?
+                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci eaque vero quae cupiditate dicta error! 
+                       Perferendis inventore excepturi cumque necessitatibus ullam, labore nesciunt ducimus deserunt mollitia nobis at praesentium illo.
+                </Testimonial>
+            </TestimonyCards>
+            <TestimonyCards>
+            <TestifierName>
+                    Solomon yalew
+                </TestifierName>
+                <Testimonial>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                    Doloremque dicta culpa deserunt 
+                    ducimus quae assumenda voluptates 
+                    illo laborum aperiam vero doloribus molestias quisquam, 
+                    eos voluptas architecto ullam nobis iste mollitia?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Quam sint commodi consequuntur repudiandae dolorem optio minima ab, 
+                    hic vitae nisi aspernatur quod ipsam, iusto alias? Quibusdam accusamus
+                     quo necessitatibus voluptates?
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Amet et expedita dolores, repudiandae iure rerum enim 
+                      quasi placeat obcaecati minima a odio natus hic quas eveniet voluptatem fugiat. Ut, ex?
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias 
+                      amet perspiciatis quas, voluptatem ea fugit, 
+                      nulla necessitatibus eaque cumque, ratione tempore omnis
+                       alias cupiditate provident voluptates molestiae possimus minus nostrum?
+                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci eaque vero quae cupiditate dicta error! 
+                       Perferendis inventore excepturi cumque necessitatibus ullam, labore nesciunt ducimus deserunt mollitia nobis at praesentium illo.
+                </Testimonial>
+            </TestimonyCards>
 
         </TestimonyDiv>
     )
