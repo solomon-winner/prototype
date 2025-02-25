58fc903a76ea691fe106d3c43a2f04a584b0efdb
solomon-winner <solyalew3@gmail.com>
2025-02-25 21:25:56 +0300

    ...state...

---
+++ src/state/state.js
@@ -0,0 +1,27 @@
+import { atom } from 'recoil';
+
+export const generalInfo = atom({
+    key: 'general',
+    default: []
+});
+
+export const cardInfoState = atom({
+  key: 'cardInfoState',
+  default: []
+});
+
+export const songsState = atom({
+  key: 'songsState',
+  default: [],
+});
+
+export const albumsState = atom({
+  key: 'albumsState',
+  default: [],
+});
+
+export const testimoniesState = atom({
+  key: 'testimoniesState',
+  default: [],
+});
+
